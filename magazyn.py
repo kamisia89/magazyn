48556258b48b97e7fd13f9b05637d9b2fec0ccc6
Kamila Kolopoleska <Kamila.eska89@gmail.com>
2023-05-10 13:15:56 +0100

    list of items

---
+++ magazyn.py
@@ -0,0 +1,5 @@
+items = ['cactus', 'daisy', 'lilac', 'rose', 'orchid', 'poppy', 'tulip', 'lotus']
+qunantity = ['999', '678', '45', '2563', '748', '1987', '77', '289']
+unit = ['pot']
+unit_price = ['2', '5.5', '12.8', '9.9', '15.5', '3.5', '8.9', '28.9']
+
