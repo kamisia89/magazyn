c31fb1c43af3c4ca3bf6a5b2f9462b4539679157
Kamila Kolopoleska <Kamila.eska89@gmail.com>
2023-05-10 13:25:30 +0100

    welcome menu

--- magazyn.py
+++ magazyn.py
@@ -1,3 +1,11 @@
+print("What would you like to do??")
+text = input()
+if text == ("exit"):
+    print ("bye, bye...")
+    exit(1)
+else:
+    print(" ***%s***" % text)
+
 items = ['cactus', 'daisy', 'lilac', 'rose', 'orchid', 'poppy', 'tulip', 'lotus']
 qunantity = ['999', '678', '45', '2563', '748', '1987', '77', '289']
 unit = ['pot']
